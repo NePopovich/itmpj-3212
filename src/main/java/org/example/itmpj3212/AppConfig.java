e246931ad7d05810ee627932ec3f0f138a628d72
Aleksey Larkov <djalekseeff@gmail.com>
2025-05-14 21:36:35 +0300

    add Task1 files

---
+++ src/main/java/org/example/itmpj3212/AppConfig.java
@@ -0,0 +1,15 @@
+package org.example.itmpj3212;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+ 
+    @Bean(name="helloworld")
+    public HelloWorld getHelloWorld() {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.setMessage("Hello World!");
+        return helloWorld;
+    }
+}
---
+++ src/main/java/org/example/itmpj3212/HelloWorld.java
@@ -0,0 +1,15 @@
+package org.example.itmpj3212;
+
+public class HelloWorld {
+ 
+    private String message;
+ 
+    public String getMessage() {
+        return message;
+    }
+ 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+     
+}
