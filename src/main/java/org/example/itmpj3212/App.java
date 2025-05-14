6975c835be95b270c2df7f9bc61eb00b9b39077d
Aleksey Larkov <djalekseeff@gmail.com>
2025-05-14 22:08:25 +0300

    Выполнена задача 1

--- src/main/java/org/example/itmpj3212/App.java
+++ src/main/java/org/example/itmpj3212/App.java
@@ -9,6 +9,14 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+//        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        Cat beanCat1 =
+                (Cat) applicationContext.getBean("cat");
+        Cat beanCat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(bean == bean1);
+        System.out.println(beanCat1 == beanCat2);
     }
 }
--- src/main/java/org/example/itmpj3212/AppConfig.java
+++ src/main/java/org/example/itmpj3212/AppConfig.java
@@ -2,6 +2,7 @@ package org.example.itmpj3212;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
@@ -11,5 +12,14 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat() {
+        Cat cat = new Cat();
+        cat.setName("Barsik");
+        cat.setAge(5);
+        return cat;
     }
 }
