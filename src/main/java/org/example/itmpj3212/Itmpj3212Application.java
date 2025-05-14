557cdf4516a834e0c8d156f65a9d53101b134ab4
Aleksey Larkov <djalekseeff@gmail.com>
2025-05-14 21:18:47 +0300

    init commit

---
+++ src/main/java/org/example/itmpj3212/Itmpj3212Application.java
@@ -0,0 +1,13 @@
+package org.example.itmpj3212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Itmpj3212Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Itmpj3212Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/itmpj3212/Itmpj3212ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.itmpj3212;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Itmpj3212ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
